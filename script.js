6bd81a87927438ff4a90c2a6956ba4e0ae15d025
allen <frazeeaw@vcu.edu>
2025-02-13 15:32:18 -0500

    added usable buttons to the piano

---
+++ script.js
@@ -0,0 +1,31 @@
+// Select all keys
+const keys = document.querySelectorAll('.key');
+
+
+//Listeners 
+// Add an event listener to all keys
+keys.forEach((key) => {
+  key.addEventListener('click', () => playNote(key));
+});
+
+
+// Handlers 
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note);
+  // Reset note audio
+  noteAudio.currentTime = 0;
+
+  // Play the note 
+  noteAudio.play();
+
+  // Add active class
+  key.classList.add('active');
+
+  // Remove the active class
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+
+  });
+}
+
+
