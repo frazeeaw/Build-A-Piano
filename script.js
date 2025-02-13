88b8a93bf6acd83914579562eaf71dd6106a1899
allen <frazeeaw@vcu.edu>
2025-02-13 16:03:29 -0500

    colors broke it all :(

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // Select all keys
 const keys = document.querySelectorAll('.key');
 
-
 //Listeners 
 // Add an event listener to all keys
 keys.forEach((key) => {
   key.addEventListener('click', () => playNote(key));
 });
-
 
 // Handlers 
 function playNote(key) {
